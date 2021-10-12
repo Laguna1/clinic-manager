c6c03acaa100a0fd8f15f487c6f15a53a12327b7
Laguna1 <lagunazoomarket@gmail.com>
2021-10-12 21:44:44 +0300

    Add references to appointment

--- db/migrate/20211012184225_create_appointments.rb
+++ db/migrate/20211012184225_create_appointments.rb
@@ -2,8 +2,10 @@ class CreateAppointments < ActiveRecord::Migration[6.1]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_date
-      t.integer :patient_id
-      t.integer :doctor_id
+      t.string :from_doctor
+      t.text :body
+      t.references :patient, null: false, foreign_key: true
+      t.references :doctor, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_182455) do
+ActiveRecord::Schema.define(version: 2021_10_12_184225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_date"
-    t.integer "patient_id"
-    t.integer "doctor_id"
+    t.string "from_doctor"
+    t.text "body"
+    t.bigint "patient_id", null: false
+    t.bigint "doctor_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -43,4 +47,6 @@ ActiveRecord::Schema.define(version: 2021_10_12_182455) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "appointments", "doctors"
+  add_foreign_key "appointments", "patients"
 end
