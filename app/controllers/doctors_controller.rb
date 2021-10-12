b4389d8fd7e59e3ad71de0bc8c898545db404627
Laguna1 <lagunazoomarket@gmail.com>
2021-10-12 23:03:07 +0300

    Add doctors controller

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,45 @@
+class DoctorsController < ApplicationController
+  before_action :set_doctor, only: %i[edit update show destroy]
+  
+  def index
+    @doctors = Doctor.all.order('created_at ASC')
+  end
+
+  def show; end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to doctor_path(@doctor)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @doctor.destroy
+    redirect_to doctors_path
+  end
+
+  def update
+    if @doctor.update(doctor_params)
+      redirect_to doctor_path(@doctor)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:firstname, :lastname, :phone_number, :email, :speciality)
+  end
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :patients
+  resources :doctors
+  resources :appointments
+
+  root 'doctors#index'
 end
